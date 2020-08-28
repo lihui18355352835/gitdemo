bb0cdee862b67e6e57710528c2b9c25627967444
Li Hui <893120619@qq.com>
2020-08-28 16:03:09 +0800

    初始化文件

---
+++ src/main/java/com/qf/gitdemo/controller/HelloFirst.java
@@ -0,0 +1,11 @@
+package com.qf.gitdemo.controller;
+
+/**
+ * @auther LiHui
+ * @Date 2020/8/28 15:57
+ */
+public class HelloFirst {
+    public static void main(String[] args) {
+        System.out.println("Hello Demo");
+    }
+}
